378f85a95d43dc9e4926e5b1183b688f595c493c
elsarlld <rolland.elsa2006@gmail.com>
2024-11-23 16:08:51 +0100

    Update header.h

--- header.h
+++ header.h
@@ -2,31 +2,11 @@
 #define HEADER_H_INCLUDED
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
-void menudujeu(){}
-//Choix Jouer ou quitter
-//Choix grille aléatoire ou manuel
-//choix dimension grille
-
-
-void sauvegarde(){}
-//Une fonction qui sauvegardera les tableaux dans d'aure tableau
-
-// Je pense qu'il n'y a pas besoin de la classe cellule vu qu'on les crées dans la grille directement.
-/*class cellule{
-private:
-    int x, y;
-    bool vivante;
-public:
-    int getX();
-    int getY();
-
-friend cellule operator+(const cellule&,const cellule&);
-friend cellule operator-(const cellule&,const cellule&);
-};*/
-
-//Il faut savoir que quand on initie la grille toute les cellules sont mortes (false)
 class grille{
 private:
     int largeur, hauteur;
@@ -36,42 +16,77 @@ public:
         cellules.resize(largeur, vector<bool>(hauteur, false)  );
     
     void configurationAleatoire(double proba);
-    bool connaitreEtat(int x, int y);//Connaitre l'état d'une cellule
+    bool isTrue(int x, int y);//Connaitre l'état d'une cellule
     void changeEtat(int x, int y, bool vivante);//Pour changer l'état de la cellule
     int scanCellule(int x, int y); //Fonction pour scanner tout autour de la cellule
     void affichage();
+    int getLargeur() const{
+        return largeur;}
+    int getHauteur() const{
+        return hauteur;}
+    //Il manque la fontion la plus importante PTDR c'est celle qui renvoie la nouvelle grille de la génération suivante après avoir fait scan cellule
+    grille generationSuivante(){}
 };
-//affichage grille, initialisation grille
-class jeu{//la classe Jeu sera plus faite pour les annexes (revenir en arrière, les boutons?, l'historique...) à ton besoin d'une classe pour cela ?
+
+
+
+class jeu{
 private:
-vector<grille> historique; //On va stocker dans un tableau les anciennes grilles (10max ?)
-int generation=0; //Compteur de génération
+    vector<grille> historique; 
+    int generation; 
 public:
-    int population();//affiche nombre cellule vivante
-    int iteration();//affiche iteration, du coup pas mieux de le faire dans la classe grille ?
-    void retour(); //affichera la dernière grille
+    jeu() : generation(0){} //On initialise la première génération à 0 càd la génération de la configuration initiale des cellules
+    void jouer(grille &grille1, int iterations); //méthode pour lancer le jeu (à appeler dans menuPrincipal)
+    int population(grille &grille1);//affiche nombre cellule vivante (à appeler dans menuPrincipal
+    void retour(); //affiche grille précédente (à appeler où??)
     int getGeneration() const {
-        return generation;
-    }
-    //void analysecellule(int x, int y); Pareille tout ce fait dans la classe Grille je pense ?
-
+        return generation;}
 };
 
-class bouton{ //On va crée plusieurs bouton en SFML mais on n'y connait pas grand chose pour le moment. On verra plus tard.
-private:
-public:
-};
 
 void grille::configurationAleatoire(double proba){
     for(int x=0; x<largeur; x++){
         for(int y=0; y<hauteur; y++){
-        cellules[x][y]= ((double)rand()/RAND_MAX) < proba;}}}
+        cellules[x][y]= ((double)rand()/RAND_MAX) < proba;}}
+}
 
-        
+void jeu::jouer(grille &grille1, iterations){
+    for(int i=0; i<iterations; i++){
+        cout<< "Génération : "<< generation<< endl;
+    grille1.affichage();
+    historique.push_back(grille1);
+    grille1= grille1.generationSuivante();
+    generation++;
+
+int jeu::population( grille &grille1){
+    int nb_vivantes=0;
+    for(int x=0; x<grille1.getLargeur; x++){
+        for(int y=0; y<grille1.getHauteur; y++){
+        if (grille1.isTrue(x, y)){
+            nb_vivantes++;}
+        }}
+}
+
+void jeu::retour(){ //chatgpt m'a dit pour.empty et .back, vous me direz si vous savez faire avec ce qu'on a vu en cours
+    if(!historique.empty()){
+    cout<<"Grille précédente : ";
+    historique.back().affichage();}
+    else {
+    cout<<"Pas de grille précédente";}}
+
+void menuPrincipal(){}
+//Choix Jouer ou quitter
+//Choix grille aléatoire ou manuel
+//choix dimension grille
+//appel grille.configurationAleatoire
+//appel jeu.jouer
+
+void sauvegarde(){}//Une fonction qui sauvegardera les tableaux dans d'autre tableau (//Pas dans un fichier???????)
+
 
 int main() {
     system("chcp 1252"); 
-    cout<< “Jeu de la vie”;
+    cout<< "Jeu de la vie";
     srand(time(0));
     menuPrincipal(); 
 return 0; }
