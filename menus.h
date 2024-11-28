4197ffe8687b5f8574119e9dea659ca95353924e
Alrylt <150608058+Alrylt@users.noreply.github.com>
2024-11-28 19:53:11 +0100

    Create menus.h

---
+++ menus.h
@@ -0,0 +1,18 @@
+#ifndef MENUS_H_INCLUDED
+#define MENUS_H_INCLUDED
+
+//void changerMenu(string );
+void ecranAurevoir();
+void demarrerLeJeu();
+void generationDeGrille();
+void importerUneGrille();
+void parametrePredefini();
+void nouveauParametre();
+void credits() ;
+void erreurMenuIndefini();
+void menuAurevoir();
+void acceuil();
+
+
+
+#endif // MENUS_H_INCLUDED
